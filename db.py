3592f45381747870c6f4a965b21f6c430db85be5
Andreas Traber <andreas.traber@transport-ninja.com>
2024-08-14 15:45:32 +0200

    add flask configuration, improve data structure

---
+++ db.py
@@ -0,0 +1,16 @@
+stores = [
+    {
+        'name': 'My Store',
+        'items': [
+            {
+                'name': 'Chair',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+items = {
+    1: {'name': 'Chair', 'price': 15.99},
+    2: {'name': 'Table', 'price': 180.50},
+}
